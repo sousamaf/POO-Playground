e4fa2e9d344057be479b86fb10ddd7ad17ce4e60
Marco Antonio <marco.volare@gmail.com>
2024-11-26 21:59:46 -0300

    feat: added files for lesson on exceptions and file manipulation

---
+++ aulas/Aula17ExceptionFiles/ManipulacaoArquivos.java
@@ -0,0 +1,20 @@
+package aulas.Aula17ExceptionFiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ManipulacaoArquivos {
+    public static void main(String[] args) {
+        String caminhoArquivo = System.getProperty("user.dir") + "/aulas/Aula17ExceptionFiles/arquivo.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro de leitura: " + e.getMessage());
+        }
+    }
+}
---
+++ aulas/Aula17ExceptionFiles/TratamentoExcecaoAritmetico.java
@@ -0,0 +1,31 @@
+package aulas.Aula17ExceptionFiles;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TratamentoExcecaoAritmetico {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Digite o dividendo: ");
+            int dividendo = sc.nextInt();
+
+            System.out.println("Digite o divisor: ");
+            int divisor = sc.nextInt();
+
+            float resultado = 0;
+
+            resultado = dividendo / divisor;
+
+            System.out.println("Resultado: " + resultado);
+
+        } catch (InputMismatchException ie) {
+            System.out.println("Digite um número inteiro.");
+
+        } catch (ArithmeticException e) {
+            System.out.println("Não se divide por zero: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
